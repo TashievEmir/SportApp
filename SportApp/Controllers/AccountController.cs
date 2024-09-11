b1d9240ccef167b3144339dc7095ececae85bb72
Tashiev Emir <tashievemir01@gmail.com>
2024-09-11 16:33:17 +0600

    created the project

---
+++ SportApp/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportApp.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    public class AccountController : ControllerBase
+    {
+
+        public AccountController() 
+        {
+        
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register()
+        {
+            return Ok();
+        }
+
+        public async Task<ActionResult> ResetPassword()
+        {
+            return Ok();
+        }
+    }
+}
