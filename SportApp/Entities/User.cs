e3640eeeab119dc5bab1f2ba6625c7710287796d
Tashiev Emir <tashievemir01@gmail.com>
2024-09-12 16:42:46 +0600

    added init migration and updated database

--- SportApp/Entities/User.cs
+++ SportApp/Entities/User.cs
@@ -11,7 +11,7 @@
         public string Photo {  get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public int RoleId { get; set; }
+        public long RoleId { get; set; }
         public Role Role { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
